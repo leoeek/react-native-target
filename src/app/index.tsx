db21c9cb5550a8aeda27193a2fc2acb9a03853bd
leoprocopio <leonardolp@gmail.com>
2025-08-02 11:00:03 -0300

    feat: flatlist targets

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import { HomeHeader } from "@/components/HomeHeader";
+import { List } from "@/components/List";
 import { Target } from "@/components/Target";
 import { View } from "react-native";
 
@@ -10,10 +11,25 @@ const summary = {
 
 const targets = [
   {
+    id: "1",
     name: "Comprar uma cadeira ergonômica",
     percentage: "75%",
     current: "900,00",
     target: "1.200,00",
+  },
+  {
+    id: "2",
+    name: "Comprar uma ventilador",
+    percentage: "15%",
+    current: "15,00",
+    target: "200,00",
+  },
+  {
+    id: "3",
+    name: "Comprar PC",
+    percentage: "5%",
+    current: "150,00",
+    target: "4.000,00",
   },
 ];
 
@@ -22,7 +38,14 @@ export default function Index() {
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
 
-      <Target data={targets[0]} />
+      <List
+        title="Metas"
+        emptyMessage="Nenhuma meta encontrada!"
+        data={targets}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Target data={item} />}
+        containerStyle={{ paddingHorizontal: 24 }}
+      />
     </View>
   );
 }
---
+++ src/components/List/index.tsx
@@ -0,0 +1,46 @@
+import {
+  FlatList,
+  FlatListProps,
+  StyleProp,
+  Text,
+  View,
+  ViewStyle,
+} from "react-native";
+import { styles } from "./Styles";
+import { Separator } from "../Separator";
+import { colors } from "@/theme";
+
+type Props<T> = FlatListProps<T> & {
+  title?: string;
+  emptyMessage?: string;
+  containerStyle?: StyleProp<ViewStyle>;
+};
+
+export function List<T>({
+  title,
+  emptyMessage,
+  containerStyle,
+  data,
+  renderItem,
+  ...rest
+}: Props<T>) {
+  return (
+    <View style={[styles.container, containerStyle]}>
+      <Text style={styles.title}>{title}</Text>
+
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        ItemSeparatorComponent={() => <Separator color={colors.gray[200]} />}
+        contentContainerStyle={styles.listContent}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.empty}>
+            {emptyMessage || "Nenhum item encontrado."}
+          </Text>
+        )}
+        {...rest}
+      />
+    </View>
+  );
+}
